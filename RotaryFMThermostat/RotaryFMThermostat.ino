4de60718b73c1f5dfc7c7ebdc48f5e1571ca720f
hannemann <hannemann666@gmx.net>
2017-04-01 10:53:04 +0200

    add radio test to Thermostat

--- RotaryFMThermostat/RotaryFMThermostat.ino
+++ RotaryFMThermostat/RotaryFMThermostat.ino
@@ -1,133 +1,70 @@
-//#define WITH_LCD 1
 
+#include "LowPower.h"
+// encoder libs
 #include <ClickEncoder.h>
 #include <TimerOne.h>
+// RFM Libs
+#include <RFM69.h>
+#include <RFM69_ATC.h>
 
-#ifdef WITH_LCD
-#include <LiquidCrystal.h>
-
-#define LCD_RS       8
-#define LCD_RW       9
-#define LCD_EN      10
-#define LCD_D4       4
-#define LCD_D5       5
-#define LCD_D6       6
-#define LCD_D7       7
-
-#define LCD_CHARS   20
-#define LCD_LINES    4
-
-LiquidCrystal lcd(LCD_RS, LCD_RW, LCD_EN, LCD_D4, LCD_D5, LCD_D6, LCD_D7);
-#endif
-
-// begin 
-#define btnPin 2
+// peripheral 
+#define BUTTON_PIN 3
+#define BUTTON_INT digitalPinToInterrupt(BUTTON_PIN)
 #define TIMOUT_TRSHLD 5000
+#define SERIAL_BAUD   115200
 
-volatile bool active = false;
-bool btnPressed = false;
-int16_t oldValue;
-unsigned long oldMillis;
-int btnInt = digitalPinToInterrupt(btnPin);
+// RFM69
+#define NODEID        1
+#define NETWORKID     100
+#define FREQUENCY     RF69_433MHZ
+#define ENCRYPTKEY    "sampleEncryptKey" //exactly the same 16 characters/bytes on all nodes!
+#define ENABLE_ATC    //comment out this line to disable AUTO TRANSMISSION CONTROL
 
+#ifdef ENABLE_ATC
+  RFM69_ATC radio;
+#else
+  RFM69 radio;
+#endif
+bool promiscuousMode = false; //set to 'true' to sniff all packets on the same network
+byte ackCount=0;
+uint32_t packetCount = 0;
+
+// encoder
 ClickEncoder *encoder;
+volatile bool active = true;
+unsigned long oldMillis;
 int16_t last, value;
 
-void timerIsr() {
-  encoder->service();
-}
-
-void buttonIsr() {
-  if (!active) {
-    activate(); 
-  }
-}
-
-#ifdef WITH_LCD
-void displayAccelerationStatus() {
-  lcd.setCursor(0, 1);  
-  lcd.print("Acceleration ");
-  lcd.print(encoder->getAccelerationEnabled() ? "on " : "off");
-}
-#endif
-
 void setup() {
-  Serial.begin(115200);
-  encoder = new ClickEncoder(A1, A0, btnPin, 4);
-
-#ifdef WITH_LCD
-  lcd.begin(LCD_CHARS, LCD_LINES);
-  lcd.clear();
-  displayAccelerationStatus();
+  Serial.begin(SERIAL_BAUD);
+  delay(10);
+  
+  radio.initialize(FREQUENCY,NODEID,NETWORKID);
+  encoder = new ClickEncoder(A1, A0, BUTTON_PIN, 4);
+  radio.encrypt(ENCRYPTKEY);
+  radio.promiscuous(promiscuousMode);
+  char buff[50];
+  sprintf(buff, "\nListening at %d Mhz...", FREQUENCY==RF69_433MHZ ? 433 : FREQUENCY==RF69_868MHZ ? 868 : 915);
+  Serial.println(buff);
+#ifdef ENABLE_ATC
+  Serial.println("RFM69_ATC Enabled (Auto Transmission Control)");
 #endif
 
   Timer1.initialize(1000);
-  Timer1.stop();
-  //Timer1.attachInterrupt(timerIsr); 
-  attachBtn();
-
-  Serial.println(btnPin);
-  Serial.println(btnInt);
-  Serial.println(pinMode(btnPin) == INPUT_PULLUP ? "PULLUP" : "OTHER");
-  
-  resetMillis();
-  
-  last = -1;
+  Serial.print("Observing button on Pin ");Serial.print(BUTTON_PIN);
+  Serial.print(" (Interrupt ");Serial.print(BUTTON_INT);Serial.println(")");
+  activate();
 }
 
 void loop() {
 
   if (active) {
-  
-    value += encoder->getValue();
+    handleRotation();
+    handleClick();
+
+    receive();
     
-    if (value != last) {
-      last = value;
-      Serial.print("Encoder Value: ");
-      Serial.println(value);
-  #ifdef WITH_LCD
-      lcd.setCursor(0, 0);
-      lcd.print("         ");
-      lcd.setCursor(0, 0);
-      lcd.print(value);
-  #endif
-    }
-    
-    ClickEncoder::Button b = encoder->getButton();
-    if (b != ClickEncoder::Open) {
-      Serial.print("Button: ");
-      btnPressed = true;
-      #define VERBOSECASE(label) case label: Serial.println(#label); break;
-      switch (b) {
-        VERBOSECASE(ClickEncoder::Pressed);
-        VERBOSECASE(ClickEncoder::Held)
-        VERBOSECASE(ClickEncoder::Released)
-        VERBOSECASE(ClickEncoder::Clicked)
-        case ClickEncoder::DoubleClicked:
-            Serial.println("ClickEncoder::DoubleClicked");
-            encoder->setAccelerationEnabled(!encoder->getAccelerationEnabled());
-            Serial.print("  Acceleration is ");
-            Serial.println((encoder->getAccelerationEnabled()) ? "enabled" : "disabled");
-  #ifdef WITH_LCD
-            displayAccelerationStatus();
-  #endif
-          break;
-      }
-    }    
-  }
-
-  if (value != oldValue) {
-    resetMillis();
-    oldValue = value;
-  }
-
-  if (btnPressed) {
-    resetMillis();
-    btnPressed = false;
-  }
-
-  if (active && timedOut()) {
-    deactivate();
+    if (timedOut()) powerDown();
   }
 }
 
@@ -138,34 +75,35 @@ void activate() {
   detachBtn();
   attachEncoder();
   resetValue();
-  active = true;
   resetMillis();
-  Serial.print("Awake after ");
-  Serial.println(oldMillis);
+  active = true;
+  Serial.println("Active...");
 }
 
 /**
- * deactivate
+ * powerDown
  */
-void deactivate() {
+void powerDown() {
   attachBtn();
   detachEncoder();
   active = false;
   Serial.println("Sleep...");
+  Serial.flush();
+  LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF); 
 }
 
 /**
  * attach button interrupt
  */
 void attachBtn() {
-  attachInterrupt(btnInt, buttonIsr, FALLING);
+  attachInterrupt(BUTTON_INT, buttonIsr, RISING);
 }
 
 /**
  * detach button interrupt
  */
 void detachBtn() {
-  detachInterrupt(btnInt);
+  detachInterrupt(BUTTON_INT);
 }
 
 /**
@@ -203,20 +141,97 @@ bool timedOut() {
  */
 void resetValue() {
   value = 0;
-  oldValue = value;
+  last = value;
 }
 
-// helper
-int pinMode(uint8_t pin)
-{
-  if (pin >= NUM_DIGITAL_PINS) return (-1);
+/**
+ * handle encoder rotation
+ */
+void handleRotation() {
 
-  uint8_t bit = digitalPinToBitMask(pin);
-  uint8_t port = digitalPinToPort(pin);
-  volatile uint8_t *reg = portModeRegister(port);
-  if (*reg & bit) return (OUTPUT);
+  value += encoder->getValue();
+  
+  if (value != last) {
+    last = value;
+    resetMillis();
+    Serial.print("Encoder Value: ");
+    Serial.println(value);
+  }
+}
 
-  volatile uint8_t *out = portOutputRegister(port);
-  return ((*out & bit) ? INPUT_PULLUP : INPUT);
+/**
+ * handle encoder click
+ */
+void handleClick() {
+  
+  ClickEncoder::Button b = encoder->getButton();
+  if (b != ClickEncoder::Open) {
+    resetMillis();
+    Serial.print("Button: ");
+    #define VERBOSECASE(label) case label: Serial.println(#label); break;
+    switch (b) {
+      VERBOSECASE(ClickEncoder::Pressed);
+      VERBOSECASE(ClickEncoder::Held)
+      VERBOSECASE(ClickEncoder::Released)
+      VERBOSECASE(ClickEncoder::Clicked)
+      case ClickEncoder::DoubleClicked:
+          Serial.println("ClickEncoder::DoubleClicked");
+          encoder->setAccelerationEnabled(!encoder->getAccelerationEnabled());
+          Serial.print("  Acceleration is ");
+          Serial.println((encoder->getAccelerationEnabled()) ? "enabled" : "disabled");
+        break;
+    }
+  } 
+}
+
+void timerIsr() {
+  encoder->service();
+}
+
+void buttonIsr() {
+  if (!active) {
+    activate(); 
+  }
+}
+
+// Radio
+void receive() {
+  
+  if (radio.receiveDone())
+  {
+    Serial.print("#[");
+    Serial.print(++packetCount);
+    Serial.print(']');
+    Serial.print('[');Serial.print(radio.SENDERID, DEC);Serial.print("] ");
+    if (promiscuousMode)
+    {
+      Serial.print("to [");Serial.print(radio.TARGETID, DEC);Serial.print("] ");
+    }
+    for (byte i = 0; i < radio.DATALEN; i++)
+      Serial.print((char)radio.DATA[i]);
+    Serial.print("   [RX_RSSI:");Serial.print(radio.RSSI);Serial.print("]");
+    
+    if (radio.ACKRequested())
+    {
+      byte theNodeID = radio.SENDERID;
+      radio.sendACK();
+      Serial.print(" - ACK sent.");
+
+      // When a node requests an ACK, respond to the ACK
+      // and also send a packet requesting an ACK (every 3rd one only)
+      // This way both TX/RX NODE functions are tested on 1 end at the GATEWAY
+      if (ackCount++%3==0)
+      {
+        Serial.print(" Pinging node ");
+        Serial.print(theNodeID);
+        Serial.print(" - ACK...");
+        delay(3); //need this when sending right after reception .. ?
+        if (radio.sendWithRetry(theNodeID, "ACK TEST", 8, 0))  // 0 = only 1 attempt, no retries
+          Serial.print("ok!");
+        else Serial.print("nothing");
+      }
+    }
+    Serial.println();
+  }
 }
 
